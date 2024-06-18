a48a770a29289353448c544ebdc56de8dc0381e3
Beko404 <abekdiyor@gmail.com>
2024-06-18 11:06:09 +0500

    Code cleared

--- app/src/main/java/uz/beko404/sharedelementtransaction/ui/DetailFragment.kt
+++ app/src/main/java/uz/beko404/sharedelementtransaction/ui/DetailFragment.kt
@@ -3,13 +3,10 @@ package uz.beko404.sharedelementtransaction.ui
 import android.os.Bundle
 import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import uz.beko404.sharedelementtransaction.R
 import uz.beko404.sharedelementtransaction.databinding.FragmentDetailBinding
 import uz.beko404.sharedelementtransaction.utill.viewBinding
-import java.util.concurrent.TimeUnit
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
     private val binding by viewBinding { FragmentDetailBinding.bind(it) }
